e347aa1d1559527b6df395ff4af15d76d49d3f85
melisagok <melisagok3@gmail.com>
2021-01-16 23:43:27 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            string[] kurslar = new string[] { "yazılım geliştirme", "programlamaya başlangıç","java" };
+            
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("sayfa sonu");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+
+            int ogrenciSayisi = 32000;
+
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("azalış butuonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("artış butuonu");
+            }
+            else
+            {
+                Console.WriteLine("eşittir butuonu");
+            }
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Ayarlar Butonu");
+            }
+            else
+            {
+                Console.WriteLine("giriş yap butonu");
+            }
+
+
+            Console.WriteLine("kategoriEtiketi");
+        }
+    }
+}
